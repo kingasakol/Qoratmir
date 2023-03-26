93c0c06ba7cb1d983af2628ab30ad9523a86790f
unknown <sakolkinga@student.agh.edu.pl>
2023-03-26 14:06:00 +0200

    tower buttons and fix in ordering

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -45,6 +45,9 @@ public class TileScript : MonoBehaviour
 
     private void PlaceTower()
     {
-        Instantiate(GameManager.Instance.TowerPrefab, transform.position, Quaternion.identity);
+        GameObject tower = (GameObject)Instantiate(GameManager.Instance.TowerPrefab, transform.position, Quaternion.identity);
+        tower.GetComponent<SpriteRenderer>().sortingOrder = GridPosition.Y;
+
+        tower.transform.SetParent(transform);
     }
 }
